df063e8f622d27beddf632cc611064355beb3045
Southworth08 <160368574+Southworth08@users.noreply.github.com>
2024-12-20 14:09:04 -0700

    Create server start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 //node src/index.js
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+console.log(path.join(__dirname, '..'));
+const initialpath = path.join(__dirname, "..");
+
+const mysql = require("mysql2");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'Qo^B(#u\Zl447.m{CzR2!9.`C>#xF.Tg',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, //set to true in production
+        httpOnly: false,
+        sameSite: 'strict',
+        maxAge: 24 * 60 * 60 * 1000 // 1 day
+    }
+}));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
