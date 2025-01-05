09b420632b6823fdec878a2f398456bd1cf9532a
Mello Mantra <mgdr0777@gmail.com>
2025-01-05 15:07:37 -0700

    fix finnicky input validation

--- public/signupScreen/app.js
+++ public/signupScreen/app.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var coords = {x: 0, y: 0};
-    var mouseMagnifier = 1;
-    let blobs = document.querySelectorAll(".blob");
-    let mouse = document.querySelector(".mouse");
+document.addEventListener("DOMContentLoaded", function () {
+    email = document.getElementById("email");
+    password = document.getElementById("password");
+    rememberbox = document.getElementById("rememberbox");
+    bademail = document.getElementById("bademail");
+    badpw = document.getElementById("badpw");
 
     // cookie
     checkCookie();
-    
+
+    // text field listeners
+    email.addEventListener("blur", checkemail);
+    password.addEventListener("blur", checkpw);
+    email.addEventListener("focus", function() {
+        email.style.boxShadow = "0 0 3px 1px rgb(77, 149, 217)"
+    });
+    password.addEventListener("focus", function() {
+        password.style.boxShadow = "0 0 3px 1px rgb(77, 149, 217)"
+    });
 });
 
-// function for matching strings with wildcard characters * = any sequence of chars, ? = any single char
-function matchWildcard(str, pattern) {
-    const regex = new RegExp('^' + pattern.replace(/\*/g, '.*').replace(/\?/g, '.') + '$');
-    return regex.test(str);
+// email validation
+function isValidEmail(str) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+[^\s@]+$/;
+    return regex.test(str);;
 }
 
 // cookie management functions
 function setCookie()
 {
-    checkCookie();
     var cname = "email";
     var cvalue = document.getElementById("email").value;
     var exdays = 1;
     var d = new Date();
     d.setTime(d.getTime()+(exdays*24*60*60*1000));
     var expires = "expires="+d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
+    document.cookie = cname+"="+cvalue+"; "+expires+"; Secure; SameSite=Strict";
 }
 
 function getCookie(cname)
@@ -52,28 +61,40 @@ function checkCookie()
 
 // login form submit function
 function submit() {
-    email = document.getElementById("email");
-    password = document.getElementById("password");
-    rememberbox = document.getElementById("rememberbox");
-    bademail = document.getElementById("bademail");
-    badpw = document.getElementById("badpw");
-
     if (rememberbox.checked) {
         setCookie();
         console.log("cookie stored. yum!");
     }
-    if (email.value.includes("@") && matchWildcard(email.value,"*.?*") && (email.value.indexOf(" ") == -1)) {
+    goodemail = checkemail();
+    goodpw = checkpw()
+    if (goodemail && goodpw) {
+        console.log("submit stuff! idk how tho")
+    }
+}
+
+// text input validation functions
+function checkemail() {
+    if (isValidEmail(email.value)) {
         bademail.style.color = "white";
-        email.classList.remove("invalid")
+        email.style.boxShadow = "none";
     } else {
         bademail.style.color = "rgb(255, 124, 124)";
-        email.classList.add("invalid")
+        bademail.innerHTML = "Please enter a valid email";
+        email.style.boxShadow = "0 0 3px 1px rgb(255, 124, 124)";
     }
+    
+    return isValidEmail(email.value);
+}
+
+function checkpw() {
     if (password.value!="") {
         badpw.style.color = "white";
-        password.classList.remove("invalid")
+        password.style.boxShadow = "none"
     } else {
+        badpw.innerHTML = "Please enter a password";
         badpw.style.color = "rgb(255, 124, 124)";
-        password.classList.add("invalid")
+        password.style.boxShadow = "0 0 3px 1px rgb(255, 124, 124)";
     }
+
+    return password.value!="";
 }
