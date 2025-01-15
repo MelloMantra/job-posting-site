2c6ff94e88f403adeb56ecccd6d6d041f136d88d
Southworth08 <160368574+Southworth08@users.noreply.github.com>
2025-01-15 19:47:04 +0000

    get-all-apps-integration

--- public/allApps/app.js
+++ public/allApps/app.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    var data;
+  
+    const jobId = window.location.pathname.split('/').pop();
+    try {
+      const response = await fetch(`../api/company/getApplications/${jobId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      if (response.ok) {
+        data = await response.json();
+        console.log(data);
+      } else {
+        console.log(`Error: ${response.status} ${response.statusText}`);
+        alert("Internal server error.");
+      } 
+    } catch (err) {
+      console.error('Error querying database:', err);
+    }
 
+    //This will get
+});
