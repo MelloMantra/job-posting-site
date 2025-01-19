a19a063054c9fd279b15617831576568e095a541
Mello Mantra <mgdr0777@gmail.com>
2025-01-18 17:29:07 -0700

    More dashboard work

--- public/companyDashboard/app.js
+++ public/companyDashboard/app.js
@@ -16,9 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // pop up menu stuff
     const jobs = document.querySelectorAll('.jobListing');
+    const moreBtns = document.querySelectorAll('.moreinfo');
+    const popUp = document.querySelectorAll('.popUp')[0];
+
+    moreBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            if (popUp.style.opacity == 0) {
+                popUp.style.top = `${button.getBoundingClientRect().top - 100}px`;
+                popUp.style.left = `${button.getBoundingClientRect().left - 175}px`;
+                popUp.style.opacity = 1;
+            } else {
+                if (popUp.getBoundingClientRect() == button.getBoundingClientRect()) {
+                    popUp.style.opacity = 0;
+                } else {
+                    popUp.style.top = `${button.getBoundingClientRect().top - 100}px`;
+                    popUp.style.left = `${button.getBoundingClientRect().left - 175}px`;
+                }
+            }
+        });
+    });
 
     for (i=0; i<jobs.length; i++) {
         
-        //addEventListener
+        //jobs[i].addEventListener('cli')
     }
 });
