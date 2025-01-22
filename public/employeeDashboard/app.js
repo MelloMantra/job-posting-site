0bac61eff0f456c09b99f152ed39a78fc7187c64
Mello Mantra <mgdr0777@gmail.com>
2025-01-22 01:29:07 -0700

    employee home done goodnight

--- public/employeeDashboard/app.js
+++ public/employeeDashboard/app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
+    // tab stuff
+    const tabButtons = document.querySelectorAll('.tabBtn');
+    const tabs = document.querySelectorAll('.tab');
+
     //get 4 jobs
     try {
         const jobs = await fetch('../api/user/get4Jobs', {
@@ -13,6 +17,49 @@ document.addEventListener('DOMContentLoaded', async () => {
             var jobsJson = await jobs.json();
 
             jobsJson = jobsJson.jobs;
+
+            const wrapper = document.querySelector(".jobsWrapper");
+            for (let i=0; i<jobsJson.length; i++) {
+                var jobListing = document.createElement("div");
+                jobListing.className = "jobListing";
+                var line1 = document.createElement("div");
+                line1.className = "line";
+                var title = document.createElement("h3");
+                title.className = "jobTitle";
+                title.classList.add("unselectable");
+                title.innerHTML = jobsJson[i].title;
+                var moreDots = document.createElement("img");
+                moreDots.className = "moreinfo";
+                moreDots.id = `job${i+1}`;
+                moreDots.src = "../assets/3dots.png";
+                line1.appendChild(title);
+                line1.appendChild(moreDots);
+                var line2 = document.createElement("div");
+                line2.className = "line";
+                var status = document.createElement("p");
+                status.className = `${jobsJson[i].status}Label`;
+                status.classList.add("unselectable");
+                status.innerHTML = jobsJson[i].status.toUpperCase();
+                var company = document.createElement("p");
+                company.innerHTML = jobsJson[i].companyName;
+                line2.appendChild(status);
+                line2.appendChild(company);
+                var line3 = document.createElement("div");
+                line3.className = "line";
+                line3.classList.add("location");
+                var locationDiv = document.createElement("div");
+                var locationIcon = document.createElement("img");
+                locationIcon.src = "../assets/location.png";
+                locationDiv.appendChild(locationIcon);
+                var locationText = document.createTextNode(" "+(jobsJson[i].address == null ? "Remote" : jobsJson[i].address));
+                locationDiv.appendChild(locationText);
+                line3.appendChild(locationDiv);
+                jobListing.appendChild(line1);
+                jobListing.appendChild(line2);
+                jobListing.appendChild(line3);
+
+                wrapper.appendChild(jobListing);
+            }
 
             /* 
             Jobs are returned in the following format:
@@ -76,10 +123,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                 status.className = `${applicationsJson[i].state}Label`;
                 status.classList.add("unselectable");
                 status.innerHTML = applicationsJson[i].state.toUpperCase();
-                var jobTitle = document.createElement("p");
-                jobTitle.innerHTML = applicationsJson[i].jobTitle;
+                var company = document.createElement("p");
+                company.innerHTML = applicationsJson[i].companyName;
                 line2.appendChild(status);
-                line2.appendChild(jobTitle);
+                line2.appendChild(company);
                 var line3 = document.createElement("div");
                 line3.className = "line";
                 line3.classList.add("location");
@@ -90,11 +137,11 @@ document.addEventListener('DOMContentLoaded', async () => {
                 var creationText = document.createTextNode(" Applied on "+formatDate(applicationsJson[i].created_at));
                 creationDiv.appendChild(creationText);
                 line3.appendChild(creationDiv);
-                applicantBox.appendChild(line1);
-                applicantBox.appendChild(line2);
-                applicantBox.appendChild(line3);
+                applicationBox.appendChild(line1);
+                applicationBox.appendChild(line2);
+                applicationBox.appendChild(line3);
 
-                wrapper.appendChild(applicantBox);
+                wrapper.appendChild(applicationBox);
             }
 
             /* 
@@ -120,95 +167,107 @@ document.addEventListener('DOMContentLoaded', async () => {
         alert('An unexpected error occurred. Please try again.');
     }
 
-// tab switching
-tabButtons.forEach(button => {
-    button.addEventListener('click', async () => {
-        await openTab(button.id.charAt(3));
+    // tab switching
+    tabButtons.forEach(button => {
+        button.addEventListener('click', async () => {
+            await openTab(button.id.charAt(3));
+        });
     });
-});
 
-// tab refreshing
-const postingsTab = document.getElementById('tab3');
-const postingsIframe = document.getElementById('allJobs');
-postingsTab.addEventListener('click', () => {
-    postingsIframe.src = "../allJobs";
-});
+    // tab refreshing
+    const appsTab = document.getElementById('tab3');
+    const appsIframe = document.getElementById('allApps');
+    appsTab.addEventListener('click', () => {
+        appsIframe.src = "../allUserApps";
+    });
 
-// pop up menu stuff
-const jobBoxes = document.querySelectorAll('.jobListing');
-const jobActionBtns = document.querySelectorAll('.jobListing img.moreinfo');
-const appActionBtns = document.querySelectorAll('.applicantBox img.moreinfo');
-const popUpActions = document.querySelectorAll('.dropdownItem');
-const popUp = document.querySelector('.popUp');
-const container = document.querySelector('.contentwrapper');
+    // pop up menu stuff
+    const jobBoxes = document.querySelectorAll('.jobListing');
+    const jobActionBtns = document.querySelectorAll('.jobListing img.moreinfo');
+    const appActionBtns = document.querySelectorAll('.applicationBox img.moreinfo');
+    const popUpActions = document.querySelectorAll('.dropdownItem');
+    const popUp = document.querySelector('.popUp');
+    const container = document.querySelector('.contentwrapper');
+    const detailsBtn = document.getElementById("detailsPop");
+    targetBox = "";
 
-document.addEventListener('click', (event) => {
-    if (!popUp.contains(event.target) && !event.target.matches('.moreinfo')) {
-        popUp.style.opacity = 0;
-    }
-});
-
-jobActionBtns.forEach(button => {
-    button.addEventListener('click', async (event) => {
-        for (i=0; i<popUpActions.length; i++) {
-            if (i==0) {continue};
-            var action = popUpActions[i];
-            action.style.cursor = "pointer";
-            if (action.id == 'detailsPop' || action.id == 'applyPop') {action.style.display = 'flex'}
-            else {
-                action.style.display = "none";
-                if (popUpActions[i-1].style.display=='flex') {popUpActions[i-1].style.borderRadius = "0px 0px 10px 10px"};
-            };
-        }
-        if (popUp.style.opacity == 0) {
-            popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
-            popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
-            await wait(100);
-            popUp.style.opacity = 1;                
+    detailsBtn.addEventListener('click', () => {
+        if (targetBox.substring(0,3)=="job") {
+            window.open("../jobDetails/job.html");
         } else {
-            if ((popUp.style.top == `${(Math.round((button.getBoundingClientRect().top + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().top + Number.EPSILON)*1000) - 20000)/1000}px`) && (popUp.style.left == `${(Math.round((button.getBoundingClientRect().left + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().left + Number.EPSILON)*1000) - 20000)/1000}px`)) {
-                popUp.style.opacity = 0;
-            } else {
+            window.open("../jobDetails/app.html");
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!popUp.contains(event.target) && !event.target.matches('.moreinfo')) {
+            popUp.style.opacity = 0;
+        }
+    });
+
+    jobActionBtns.forEach(button => {
+        button.addEventListener('click', async (event) => {
+            targetBox = button.id;
+            for (i=0; i<popUpActions.length; i++) {
+                if (i==0) {continue};
+                var action = popUpActions[i];
+                action.style.cursor = "pointer";
+                if (action.id == 'detailsPop' || action.id == 'applyPop') {action.style.display = 'flex'}
+                else {action.style.display = "none"};
+            }
+            if (popUp.style.opacity == 0) {
                 popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
                 popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
-            }
-        }
-        event.stopPropagation();
-    });
-});
-
-appActionBtns.forEach(button => {
-    button.addEventListener('click', async (event) => {
-        for (i=0; i<popUpActions.length; i++) {
-            if (i==0) {continue};
-            var action = popUpActions[i];
-            if (action.id == 'detailsPop' || action.id == 'editPop' || action.id == 'withdrawPop') {action.style.display = 'flex'}
-            else {action.style.display = "none"};
-        }
-        if (popUp.style.opacity == 0) {
-            popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
-            popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
-            await wait(100);
-            popUp.style.opacity = 1;                
-        } else {
-            if ((popUp.style.top == `${(Math.round((button.getBoundingClientRect().top + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().top + Number.EPSILON)*1000) - 20000)/1000}px`) && (popUp.style.left == `${(Math.round((button.getBoundingClientRect().left + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().left + Number.EPSILON)*1000) - 20000)/1000}px`)) {
-                popUp.style.opacity = 0;
+                await wait(100);
+                popUp.style.opacity = 1;                
             } else {
+                if ((popUp.style.top == `${(Math.round((button.getBoundingClientRect().top + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().top + Number.EPSILON)*1000) - 20000)/1000}px`) && (popUp.style.left == `${(Math.round((button.getBoundingClientRect().left + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().left + Number.EPSILON)*1000) - 20000)/1000}px`)) {
+                    popUp.style.opacity = 0;
+                } else {
+                    popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
+                    popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
+                }
+            }
+            event.stopPropagation();
+        });
+    });
+
+    appActionBtns.forEach(button => {
+        button.addEventListener('click', async (event) => {
+            targetBox = button.id;
+            for (i=0; i<popUpActions.length; i++) {
+                if (i==0) {continue};
+                var action = popUpActions[i];
+                if (action.id == 'detailsPop' || action.id == 'editPop' || action.id == 'withdrawPop') {action.style.display = 'flex'}
+                else {
+                    action.style.display = "none";
+                    if (popUpActions[i-1].style.display=='flex') {popUpActions[i-1].style.borderRadius = "0px 0px 10px 10px"};
+                };
+            }
+            if (popUp.style.opacity == 0) {
                 popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
                 popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
+                await wait(100);
+                popUp.style.opacity = 1;                
+            } else {
+                if ((popUp.style.top == `${(Math.round((button.getBoundingClientRect().top + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().top + Number.EPSILON)*1000) - 20000)/1000}px`) && (popUp.style.left == `${(Math.round((button.getBoundingClientRect().left + Number.EPSILON)*1000) - Math.round((container.getBoundingClientRect().left + Number.EPSILON)*1000) - 20000)/1000}px`)) {
+                    popUp.style.opacity = 0;
+                } else {
+                    popUp.style.top = `${button.getBoundingClientRect().top - container.getBoundingClientRect().top - 20}px`;
+                    popUp.style.left = `${button.getBoundingClientRect().left - container.getBoundingClientRect().left - 20}px`;
+                }
             }
-        }
-        event.stopPropagation();
+            event.stopPropagation();
+        });
     });
-});
 
-// romir's profile code
-employeeData = {
-    fullName: "john deere",
-    email: "contact@techcorp.com",
-    description: "Leading provider of innovative software solutions..."
-};
-initializeProfile();
+    // romir's profile code
+    employeeData = {
+        fullName: "john deere",
+        email: "contact@techcorp.com",
+        description: "Leading provider of innovative software solutions..."
+    };
+    initializeProfile();
 });
 
 function formatDate(dateStr) {
